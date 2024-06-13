e0f033306318c997d793269ef0f578c858840151
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2024-06-13 16:23:00 -0700

    Adds: tailwind configs and prettier and site font

--- jacob-saltzman/src/app/layout.tsx
+++ jacob-saltzman/src/app/layout.tsx
@@ -1,5 +1,14 @@
+import "./globals.css";
+import { Fira_Code } from 'next/font/google'
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
+
+const fcFont = Fira_Code({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-fcFont',
+  weight: "400"
+});
 
 export default function RootLayout({
   children,
@@ -8,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={fcFont.variable}>{children}</body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
