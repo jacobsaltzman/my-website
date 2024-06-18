178afecc116e5b993b9f81f26aed0eec5704ce80
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2024-06-17 17:35:15 -0700

    Adds: logo svg and header plus footer boilerplate and fixes font.

--- jacob-saltzman/src/app/layout.tsx
+++ jacob-saltzman/src/app/layout.tsx
@@ -1,13 +1,15 @@
 import "./globals.css";
-import { Fira_Code } from 'next/font/google'
+import { Fira_Code } from "next/font/google";
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const fcFont = Fira_Code({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-fcFont',
-  weight: "400"
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-fcFont",
+  weight: "400",
 });
 
 export default function RootLayout({
@@ -16,8 +18,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={fcFont.variable}>{children}</body>
+    <html lang="en" className={fcFont.variable}>
+      <body className="bg-[#070815] text-white">
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
---
+++ jacob-saltzman/src/components/Header.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Logo from "@/components/Logo";
+
+export default function Header() {
+  return <div><Logo /></div>;
+}
