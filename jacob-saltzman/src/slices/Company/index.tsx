c2c1bb79edaf8b1e57d0d04f47fa303170d37ed5
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2024-07-08 18:40:37 -0700

    Returns the formatting back to the original grid structure instead of the alternating.

--- jacob-saltzman/src/slices/Company/index.tsx
+++ jacob-saltzman/src/slices/Company/index.tsx
@@ -37,77 +37,46 @@ const Company = async ({ slice }: CompanyProps): Promise<JSX.Element> => {
       <h2 className="max-w-2xl text-balance text-center text-5xl font-medium md:text-7xl">
         <PrismicText field={slice.primary.heading} />
       </h2>
+
       <div className="mx-auto mt-6 max-w-md text-balance text-center text-slate-300">
         <PrismicRichText field={slice.primary.body} />
       </div>
 
       <div className="mt-20 grid gap-16">
-        {companyDetails.map((company, index) =>
-          company ? (
-            <div
-              key={company.id}
-              className="relative grid gap-4 opacity-85 transition-opacity duration-300 hover:cursor-pointer hover:opacity-100 md:grid-cols-3 md:gap-8"
-            >
-              {/* Conditional rendering to alternate columns */}
-              {index % 2 === 0 ? (
-                <>
-                  {/* Text in the left column */}
-                  <div className="col-span-1 flex flex-col justify-center gap-4">
-                    <h3 className="text-4xl">
-                      <PrismicText field={company.data.company} />
-                    </h3>
-                    <div className="max-w-md">
-                      <PrismicRichText field={company.data.description} />
-                    </div>
-                    <PrismicNextLink
-                      document={company}
-                      className="after:absolute after:inset-0 hover:underline"
-                    >
-                      Read About My <PrismicText field={company.data.company} />{" "}
-                      Experience
-                    </PrismicNextLink>
+        {companyDetails.map(
+          (company, index) =>
+            company && (
+              <div
+                key={company.id}
+                className="relative grid gap-4 opacity-85 transition-opacity duration-300 hover:cursor-pointer hover:opacity-100 md:grid-cols-2 md:gap-8 lg:grid-cols-3"
+              >
+                <div className="col-span-1 flex flex-col justify-center gap-4">
+                  <h3 className="text-4xl">
+                    <PrismicText field={company.data.company} />
+                  </h3>
+                  <div className="max-w-md">
+                    <PrismicRichText field={company.data.description} />
                   </div>
-                  {/* Logo in the center column */}
-                  <PrismicNextImage
-                    field={company.data.logo_image}
-                    quality={100}
-                    sizes="(max-width: 768px) 100vw, 50vw"
-                    className="col-span-1 mx-auto rounded-xl max-h-44 max-w-44"
-                  />
-                  {/* Empty right column */}
-                  <div className="col-span-1"></div>
-                </>
-              ) : (
-                <>
-                  {/* Empty left column */}
-                  <div className="col-span-1"></div>
-                  {/* Logo in the center column */}
-                  <PrismicNextImage
-                    field={company.data.logo_image}
-                    quality={100}
-                    sizes="(max-width: 768px) 100vw, 50vw"
-                    className="col-span-1 mx-auto rounded-xl max-h-44 max-w-44"
-                  />
-                  {/* Text in the right column */}
-                  <div className="col-span-1 flex flex-col justify-center gap-4">
-                    <h3 className="text-4xl">
-                      <PrismicText field={company.data.company} />
-                    </h3>
-                    <div className="max-w-md">
-                      <PrismicRichText field={company.data.description} />
-                    </div>
-                    <PrismicNextLink
-                      document={company}
-                      className="after:absolute after:inset-0 hover:underline"
-                    >
-                      Read About My <PrismicText field={company.data.company} />{" "}
-                      Experience
-                    </PrismicNextLink>
-                  </div>
-                </>
-              )}
-            </div>
-          ) : null,
+
+                  <PrismicNextLink
+                    document={company}
+                    className="after:absolute after:inset-0 hover:underline"
+                  >
+                    Read About My <PrismicText field={company.data.company} />{" "}
+                    Experience
+                  </PrismicNextLink>
+                </div>
+                <PrismicNextImage
+                  field={company.data.logo_image}
+                  quality={100}
+                  sizes="(max-width: 768px) 100vw, 50vw"
+                  className={clsx(
+                    "rounded-xl lg:col-span-2",
+                    index % 2 && "md:-order-1"
+                  )}
+                />
+              </div>
+            )
         )}
       </div>
     </Bounded>
