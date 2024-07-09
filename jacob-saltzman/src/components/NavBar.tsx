1999cde9ecb10bf3ac72de9f81b65932a019e2f2
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2024-07-08 17:55:45 -0700

    Fixes: home link logo on mobile navbar

--- jacob-saltzman/src/components/NavBar.tsx
+++ jacob-saltzman/src/components/NavBar.tsx
@@ -21,7 +21,7 @@ export default function NavBar({ settings }: NavBarProps) {
     <nav className="px-4 py-4 md:px-6 md:py-6" aria-label="Main">
       <div className="mx-auto flex max-w-6xl flex-col justify-between py-2 font-medium text-white md:flex-row md:items-center">
         <div className="flex items-center justify-between">
-          <Link href={"/"}>
+          <Link href="/" className="z-50" onClick={() => setOpen(false)}>
             <Logo />
             <span className="sr-only">saltzman.dev logo link to homepage</span>
           </Link>
